0cd3e8fe158ce92aa2b8f0856cea71cc1f513abc
Sendel <sendel@sendel.ru>
2019-05-07 00:04:50 +0500

    comments

--- src/main/java/main/TodoController.java
+++ src/main/java/main/TodoController.java
@@ -7,23 +7,25 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import response.Todo;
 
 @RestController
 public class TodoController {
 
+   //get list
    @GetMapping("/todos/")
    public List<Todo> list() {
       return Storage.getAllTodos();
    }
 
+   //add to do
    @PostMapping("/todos/")
    public int add(Todo todo) {
       return Storage.addTodo(todo);
    }
 
+   //get item
    @GetMapping("/todos/{id}")
    public ResponseEntity<Object> get(@PathVariable int id) {
       Todo todo = Storage.getTodo(id);
@@ -33,6 +35,7 @@ public class TodoController {
       return new ResponseEntity<>(todo, HttpStatus.OK);
    }
 
+   //delete
    @DeleteMapping("/todos/{id}")
    public ResponseEntity<Object> delete(@PathVariable int id) {
       if (Storage.deleteTodo(id)) {
